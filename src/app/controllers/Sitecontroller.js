17b16a51378738d455f3f69687689ba542d1be3d
Cuong Vo <thanhcuong1303>
2021-11-10 09:35:14 +0700

    add folder controllers and routes

---
+++ src/app/controllers/Sitecontroller.js
@@ -0,0 +1,14 @@
+class SiteControllers {
+
+    //[GET] /
+    index(req, res) {
+        res.render('home');
+    }
+
+    //[GET] /search
+    search(req, res) {
+        res.render('search');
+    }
+}
+
+module.exports = new SiteControllers();
--- src/index.js
+++ src/index.js
@@ -5,10 +5,16 @@ const handlebars  = require('express-handlebars');
 
 const app = express()
 const port = 3000
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
 
 // HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // Template engine
 app.engine('hbs', handlebars({
@@ -17,14 +23,10 @@ app.engine('hbs', handlebars({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-//route
-app.get('/', (req, res) => {
-  res.render('home');
-})
 
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+// action --> dispatcher --> function handler
+//route
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
